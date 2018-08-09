e6d9bd7d5d78537f8b601dff070290c9c3f2bf6a
Howellp80 <howellp@oregonstate.edu>
2018-08-09 15:45:09 -0700

    Update databaseInit.sql

--- databaseInit.sql
+++ databaseInit.sql
@@ -43,7 +43,7 @@ CREATE TABLE `users` (
 
 -- Add testeing values to Users table
 INSERT INTO `users` VALUES (00000000000, "Adam", "Lowd", "alowd@gmail.com", "adamspass", PointFromText("POINT(24.44770 14.69106)"), "2018-08-02 16:33:18"),
-									(00000000001, "Michael", "Fern", "ferndog@hotmail.com", "FernToWin73", PointFromText("POINT(121.45778 24.69106)"), "2017-10-10 12:13:59");
+			   (00000000001, "Michael", "Fern", "ferndog@hotmail.com", "FernToWin73", PointFromText("POINT(121.45778 24.69106)"), "2017-10-10 12:13:59");
 
 
 
