29428070a67021f681cd67158d53f43c78ce79a9
Daniel Beckham <beckhamdaniels@gmail.com>
2018-08-12 23:46:02 -0500

    Add Passport.js registration system

--- app.js
+++ app.js
@@ -1,44 +1,266 @@
-var express     = require("express"), 
-    app         = express(),
-    handlebars  = require("express-handlebars").create({defaultLayout:'main'}),
-    bodyParser  = require("body-parser");
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars').create({ defaultLayout:'main' });
+const session  = require('express-session');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { check, validationResult } = require('express-validator/check');
+const flash = require('connect-flash');
+const path = require("path");
+const mysql = require('mysql');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
-//var mysql       = require('./dbcon.js')
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'user',
+  password: 'password',
+  database: 'database'
+});
 
-const apiKey = 'AIzaSyBg3NBblmeFyPMZVY1ZMCVcN4pv6Nbtf2A';    
-app.use(bodyParser.urlencoded({ extended: true }));
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
-app.use(express.static("public"));
+app.use(cookieParser());
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(flash());
 
-//app.set('mysql', mysql);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 3000);
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Passport session setup (serialize user)
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+// Passport session setup (deserialize user)
+
+passport.deserializeUser(function(id, done) {
+  pool.query("SELECT * FROM users WHERE id = ? ",[id], function(err, rows) {
+    done(err, rows[0]);
+  });
+});
+
+// Passport local strategy (sign up)
+
+passport.use('local-signup', new LocalStrategy({
+  usernameField: 'username',
+  passwordField: 'password',
+  passReqToCallback: true
+}, function(req, username, password, done) {
+  pool.query("SELECT * FROM users WHERE username = ?", [username], function(err, rows) {
+    if (err) {
+      return done(err);
+    }
+
+    if (rows.length) {
+      return done(null, false, req.flash('error', 'Username already taken'));
+    } else {
+      var newUserMysql = {
+        username: username,
+        password: bcrypt.hashSync(password, 10)
+      };
+
+      var insertQuery = "INSERT INTO users (username, password) values (?,?)";
+
+      pool.query(insertQuery, [newUserMysql.username, newUserMysql.password], function(err, rows) {
+        newUserMysql.id = rows.insertId;
+        return done(null, newUserMysql);
+      });
+    }
+  });
+}));
+
+// Passport local strategy (sign in)
+
+passport.use('local-signin', new LocalStrategy({
+  usernameField: 'username',
+  passwordField: 'password',
+  passReqToCallback: true
+}, function(req, username, password, done) {
+  pool.query("SELECT * FROM users WHERE username = ?", [username], function(err, rows) {
+    if (err) {
+      return done(err);
+    }
+
+    if (!rows.length) {
+      return done(null, false, req.flash('error', 'No such username'));
+    }
+
+    if (!bcrypt.compareSync(password, rows[0].password)) {
+      return done(null, false, req.flash('error', 'Incorrect password'));
+    }
+
+    return done(null, rows[0]);
+  });
+}));
+
+// Home (GET)
+
 app.get('/', function(req, res) {
-   res.render("index");
+  res.render('index', {
+    user: req.user,
+    successMessage: req.flash('success')
+  });
 });
 
-//handling errors
-app.use(function(req,res){
-  res.status(404);
-  res.render('404');
+// Sign in (GET)
+
+app.get('/signin', function(req, res) {
+  if (req.isAuthenticated()) {
+    res.redirect('account');
+  } else {
+    res.render('signin', {
+      errorMessage: req.flash('error')
+    });
+  }
 });
 
-app.use(function(err, req, res, next){
+// Sign in (POST)
+
+app.post('/signin', passport.authenticate('local-signin', {
+  successRedirect: '/',
+  failureRedirect: '/signin',
+  successFlash: 'Successfully signed in',
+  failureFlash: true
+}), function (req, res) {
+  if (req.body.remember) {
+    req.session.cookie.maxAge = 60000;
+  } else {
+    req.session.cookie.expires = false;
+  }
+
+  res.redirect('/');
+});
+
+// Sign up (GET)
+
+app.get('/signup', function(req, res) {
+  if (req.isAuthenticated()) {
+    res.redirect('account');
+  } else {
+    res.render('signup', {
+      errorMessage: req.flash('error')
+    });
+  }
+});
+
+// Sign up (POST)
+
+app.post('/signup',
+  check('password')
+    .isLength({ min: 4 }).withMessage('Password must be at least 4 characters in length'),
+  check('username')
+    .isLength({ min: 2 }).withMessage('Username must be at least 2 characters in length')
+    .isAlphanumeric().withMessage('Username must be alphanumeric'),
+  function(req, res, next) {
+    var errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      var errors = errors.array().map(function(error) {
+          return error.msg;
+      });
+
+      req.flash('error', errors.join('<br \>'));
+      res.redirect('/signup');
+    } else {
+      next();
+    }
+  },
+  passport.authenticate('local-signup', {
+    successRedirect: '/',
+    failureRedirect: '/signup',
+    successFlash: 'Account successfully created',
+    failureFlash: true
+  })
+);
+
+// Account (GET)
+
+app.get('/account', function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render('account', {
+      user: req.user,
+      successMessage: req.flash('success'),
+      errorMessage: req.flash('error')
+    });
+  } else {
+    res.redirect('/signin');
+  }
+});
+
+// Change password (POST)
+
+app.post('/account/password',
+  check('password')
+    .isLength({ min: 4 }).withMessage('Password must be at least 4 characters in length')
+    .custom(function(value, { req, loc, path }) {
+      if (value !== req.body.confirmPassword) {
+          return false;
+      } else {
+          return value;
+      }
+    }).withMessage('Passwords do not match'),
+  function(req, res) {
+    var errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      var errors = errors.array().map(function(error) {
+          return error.msg;
+      });
+
+      req.flash('error', errors.join('<br \>'));
+      res.redirect('/account');
+    } else {
+      var updateQuery = "UPDATE users SET password = ? WHERE id = ?";
+
+      pool.query(updateQuery, [bcrypt.hashSync(req.body.password, 10), req.user.id], function(err, rows) {
+        if (err) {
+          return next(err);
+        }
+
+        req.flash('success', 'Password successfully changed');
+        res.redirect('/account');
+      });
+    }
+  }
+);
+
+// Sign out (GET)
+
+app.get('/signout', function(req, res) {
+  req.logout();
+  req.flash('success', 'Successfully signed out');
+  res.redirect('/');
+});
+
+app.use(function(req, res) {
+  res.status(404).send('404 Not Found');
+});
+
+app.use(function(err, req, res, next) {
   console.error(err.stack);
-  res.type('plain/text');
-  res.status(500);
-  res.render('500');
+  res.status(500).send('500 Internal Server Error');
 });
 
-//use this section to test on cloud9
-//app.listen(process.env.PORT, process.env.IP, function() {
-//   console.log("Server is up"); 
-//});
+// Listen on specified port
 
-//use this section to test on localhost
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function() {
   console.log('Express started on port ' + app.get('port') + '; press Ctrl-C to terminate.');
 });
---
+++ database.sql
@@ -0,0 +1,63 @@
+/****************************************************************************************
+* Parker Howell
+* Aug. 9th, 2018
+* SQL Database Initialization for Client Project
+* Adds two testing entries to Users table for testing...
+****************************************************************************************/
+
+
+-- Usage examples for website:
+
+
+-- To add a new user
+-- INSERT INTO `users` (`fname`, `lname`, `email`, `pass`, `geo`, `created`) VALUES ("Paul", "Staments", "shroomhat@errowind.com", "Imafungi", PointFromText("POINT(69 69)"), "2016-02-02 06:12:58");
+
+
+-- To get a user's geolocation coordinates
+-- SELECT `geo` FROM `users` WHERE `fname` = "Adam";
+-- returns = POINT(24.4477 14.69106)
+
+
+-- To update a user's password based on first and last name
+-- UPDATE `users` SET `password` = "NewPass" WHERE `fname` = "Adam" AND `lname` = "Lowd";
+
+
+-- Delete existing tables
+DROP TABLE IF EXISTS `users`;
+-- DROP TABLE IF EXISTS `assistance_req`;
+-- DROP TABLE IF EXISTS `request_resp`;
+-- DROP TABLE IF EXISTS `alerts`;
+
+
+-- Create the Users table
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `fname` varchar(100) NOT NULL,
+  `lname` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `geolocation` point NOT NULL,
+  `created` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- Add testing values to Users table
+INSERT INTO `users` (`id`, `username`, `password`, `fname`, `lname`, `email`, `geolocation`, `created`) VALUES
+(1, "alowd", "$2a$10$XQxn53ey.VCU/dc58nt72u2GvjXXn2Wjvtb0qwziqHJN8BvhTuCVS", "Adam", "Lowd", "alowd@gmail.com", PointFromText("POINT(24.44770 14.69106)"), "2018-08-02 16:33:18");
+(2, "alowd", "$2a$10$nG/UF0lODWsD38jU8CBWjezNF8GoJFG9vjFfw3tlgcwiekVIkdxZ2", "Michael", "Fern", "ferndog@hotmail.com", PointFromText("POINT(121.45778 24.69106)"), "2017-10-10 12:13:59");
+
+
+/* adjust and add later if needed...
+-- Create the Assistance Request table
+CREATE TABLE `assistance_req` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`fname` varchar(100) NOT NULL,
+	`lname` varchar(100) NOT NULL,
+	`email` varchar(100) NOT NULL,
+	`geo`	point NOT NULL, 
+	`created` datetime NOT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+*/
