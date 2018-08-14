ebdfa75e7dec3ca52d078e0719a2f9c37420d34d
Louie Nicastro <louis.m.nicastro@gmail.com>
2018-08-14 19:18:38 +0000

    add buttons to drive map functions to screen (functions NOT created - just dummy for now). Implement custom map icon for electric hazard to appear on-click on map

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,11 +1,60 @@
-function myMap() {
-  var mapCanvas = document.getElementById("map");
-  var myCenter = new google.maps.LatLng(39.61114, -75.689443); 
-  var mapOptions = {center: myCenter, zoom: 15};
-  var map = new google.maps.Map(mapCanvas,mapOptions);
-  var marker = new google.maps.Marker({
-    position: myCenter,
-    animation: google.maps.Animation.BOUNCE
+var map;
+var stdMarkers = [];
+var floodMarkers = [];
+
+      
+function initMap() {
+  
+  var startingPoint = {lat: 39.61114, lng: -75.689443};
+  map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 16,
+          center: startingPoint,
+          mapTypeId: 'terrain'
+        });
+        
+  // This event listener will call addMarker() when the map is clicked.
+  map.addListener('click', function(event) {
+    addMarkerFlood(event.latLng);
   });
-  marker.setMap(map);
+  
+  //initialize the center mark
+  addMarkerStandard(startingPoint);
+ 
+}
+
+// Adds a marker to the map and push to the array.
+function addMarkerStandard(location) {
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    animation: google.maps.Animation.DROP
+  });
+  stdMarkers.push(marker);
+}
+
+// Adds a marker to the map and push to the array.
+function addMarkerFlood(location) {
+  var floodIcon = {
+    url: './icons/electric.png',
+    // This marker is 20 pixels wide by 32 pixels high.
+    //size: new google.maps.Size(20, 32),
+    scaledSize: new google.maps.Size(50, 50),
+    // The origin for this image is (0, 0).
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(25, 25)
+  };
+
+  var shape = {
+    coords: [1, 1, 1, 20, 18, 20, 18, 1],
+    type: 'poly'
+  };
+  
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    icon: floodIcon,
+    shape: shape,
+    animation: google.maps.Animation.BOUNCE,
+  });
+  floodMarkers.push(marker);
 }
